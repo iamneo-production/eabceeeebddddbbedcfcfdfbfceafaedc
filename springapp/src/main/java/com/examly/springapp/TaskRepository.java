dbe93565931f7ec83c7d7f9d719b9a2bbd039dd7
Iamneo <techadmin@iamneo.ai>
2023-08-13 20:34:25 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/TaskRepository.java
+++ springapp/src/main/java/com/examly/springapp/TaskRepository.java
@@ -3,4 +3,6 @@ import com.examly.springapp.*;
 import org.springframework.data.repository.CrudRepository;
 public interface TaskRepository extends CrudRepository<Task, Long> {
     Task findByTaskHolderName(String taskHolderName);
+    Task findById(int id);
+    Task orElse();
 }
